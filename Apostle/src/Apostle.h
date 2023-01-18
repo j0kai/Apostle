de4780f80ace3558ce2c6f8bd6dcefccad5e5492
Kyle Jones <kyjo1308@gmail.com>
2023-01-18 14:28:59 +0000

    Created premake files for project

--- Apostle/src/Apostle.h
+++ Apostle/src/Apostle.h
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include "Apostle/Application.h"
+#include "Apostle/Log.h"
 
 // ---Entry Point------------------
 #include "Apostle/EntryPoint.h"
--- Apostle/src/Apostle/EntryPoint.h
+++ Apostle/src/Apostle/EntryPoint.h
@@ -6,7 +6,10 @@ extern Apostle::Application* Apostle::CreateApplication();
 
 int main(int argc, char** argv)
 {
-	printf("Apostle Engine");
+	Apostle::Log::Init();
+	AP_CORE_TRACE("Initialised Log!");
+	AP_TRACE("Initialised Log!");
+
 	auto app = Apostle::CreateApplication();
 	app->Run();
 	delete app;
