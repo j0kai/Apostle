511137c436d703aa2ef74f3b32a093058840d5bf
Kyle Jones <105503076+j0kai@users.noreply.github.com>
2025-07-28 14:48:53 +0100

    added pop up menu when right-clicking component names in details panel

--- ApostleEditor/src/Panels/SceneHierarchyPanel.cpp
+++ ApostleEditor/src/Panels/SceneHierarchyPanel.cpp
@@ -44,7 +44,7 @@ namespace Apostle {
 
 		ImGui::End();
 
-		ImGui::Begin("Inspector");
+		ImGui::Begin("Details");
 
 		if (m_SelectionContext)
 		{
@@ -374,7 +374,7 @@ namespace Apostle {
 			bool removeComponent = false;
 			if (isRemovable)
 			{
-				if (ImGui::BeginPopup("ComponentSettings"))
+				if (ImGui::BeginPopupContextWindow(0, ImGuiPopupFlags_MouseButtonRight) || ImGui::BeginPopup("ComponentSettings"))
 				{
 					if (ImGui::MenuItem("Remove Component"))
 					{
